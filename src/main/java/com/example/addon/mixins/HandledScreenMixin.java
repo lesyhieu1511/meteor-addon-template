cae09b18d4e502e32be74342c7ed660cddcfc0bd
lesyhieu1511 <lesyhieu1511@gmail.com>
2026-09-07 19:36:18 +0700

    Add inventory hovered-slot accessor

---
+++ src/main/java/com/example/addon/mixins/HandledScreenMixin.java
@@ -0,0 +1,12 @@
+package com.example.addon.mixins;
+
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.world.inventory.Slot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(AbstractContainerScreen.class)
+public interface HandledScreenMixin {
+    @Accessor("hoveredSlot")
+    Slot addon$getFocusedSlot();
+}
