c15e3720e11bdc6fac4955c6c5ccdf01c9f50964
lesyhieu1511 <lesyhieu1511@gmail.com>
2026-09-07 20:45:36 +0700

    Fix HandledScreenMixin accessor for MC 26.2

--- src/main/java/com/example/addon/mixin/HandledScreenMixin.java
+++ src/main/java/com/example/addon/mixin/HandledScreenMixin.java
@@ -3,16 +3,10 @@ package com.example.addon.mixin;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(AbstractContainerScreen.class)
-public abstract class HandledScreenMixin {
-    @Shadow
-    public abstract Slot getHoveredSlot();
-
-    @Unique
-    public Slot addon$getFocusedSlot() {
-        return getHoveredSlot();
-    }
+public interface HandledScreenMixin {
+    @Accessor("hoveredSlot")
+    Slot addon$getFocusedSlot();
 }
